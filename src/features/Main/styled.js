b33ab5cdc56cc97182094d717b737fefe4476f31
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 15:09:42 +0200

    added styles for StyledMain

---
+++ src/features/Main/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+	padding-top: 200px;
+	height: 100%;
+	width: 65%;
+	background-color: #e1e1e1;
+	overflow: hidden;
+`;
