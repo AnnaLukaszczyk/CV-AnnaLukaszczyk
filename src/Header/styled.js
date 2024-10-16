ffb0214690708e013573b50c3cf66c790c0f20d2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 14:05:32 +0200

    added "fortawesome" import and styled for Data, PhotoBackground, Photo, NameAndContact, Name, Icon, Mail, PhoneNumber

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const StyledHeader = styled.header`
 	position: absolute;
@@ -47,4 +48,58 @@ export const Layer3 = styled(Layer)`
 		rgba(57, 124, 144, 0.01) 100%
 	);
 	clip-path: polygon(0 0, 100% 0, 100% 30%, 64% 96%, 0% 50%);
+`;
+
+export const Data = styled.div`
+	position: absolute;
+	display: flex;
+	left: 56px;
+	top: 20px;
+`;
+
+export const PhotoBackground = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 186px;
+	width: 150px;
+	border-radius: 3px;
+	background: linear-gradient(
+		90deg,
+		rgba(146, 203, 221, 1) 0%,
+		rgba(34, 89, 105, 1) 100%
+	);
+`;
+
+export const Photo = styled.img`
+	height: 180px;
+	width: 144px;
+`;
+
+export const NameAndContact = styled.div`
+	margin-left: 100px;
+	color: azure;
+`;
+
+export const Name = styled.h1`
+	margin-top: 16px;
+	font-size: 32px;
+	font-weight: bold;
+	text-transform: uppercase;
+	letter-spacing: 1px;
+	text-shadow: 1px 1px 2px rgb(26, 26, 26);
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+	margin-right: 10px;
+	filter: drop-shadow(1px 1px 2px rgb(26, 26, 26));
+`;
+
+export const Mail = styled.p`
+	margin: 10px 0;
+	text-shadow: 1px 1px 2px rgb(26, 26, 26);
+`;
+
+export const PhoneNumber = styled.p`
+	text-shadow: 1px 1px 2px rgb(26, 26, 26);
 `;
