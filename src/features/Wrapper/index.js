82c43e614d964d59fc4f89afd8ee99cf1d6448c4
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 15:09:19 +0200

    added Main component to Wrapper

--- src/features/Wrapper/index.js
+++ src/features/Wrapper/index.js
@@ -1,8 +1,10 @@
 import { StyledWrapper } from "./styled";
 import { Header } from "../Header";
+import { Main } from "../Main";
 
 export const Wrapper = () => (
 	<StyledWrapper>
 		<Header />
+		<Main />
 	</StyledWrapper>
 );
