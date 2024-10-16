86b578707067652a0edf7517aa9ea35f50589d27
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 16:15:43 +0200

    added styles for StyledAside

---
+++ src/features/Aside/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledAside = styled.aside`
+	padding-top: 230px;
+	height: 100%;
+	width: 35%;
+	box-shadow: 2px 0 3px gray;
+	overflow: hidden;
+	z-index: 1;
+`;
