8525221f3e1428d50b9c819bd3362bbae7ccfee9
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 17:11:43 +0200

    added "content" in Section component

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,9 +1,11 @@
+import { List } from "../List";
 import { SectionTitle, SectionTitleShadow, StyledSection } from "./styled";
 
-export const Section = ({ name, isMain }) => (
+export const Section = ({ name, isMain, content }) => (
 	<StyledSection>
 		<SectionTitleShadow>
 			<SectionTitle $isMain={isMain}>{name}</SectionTitle>
 		</SectionTitleShadow>
+		<List content={content} />
 	</StyledSection>
 );
