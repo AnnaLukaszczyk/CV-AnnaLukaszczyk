d29e0132a4167a58d6ffd360f35ef27255ff72cb
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 15:16:41 +0200

    created Main component

---
+++ src/features/Main/index.js
@@ -0,0 +1,10 @@
+import { Section } from "../../common/Section";
+import { StyledMain } from "./styled";
+
+export const Main = () => (
+	<StyledMain>
+		<Section name="Edukacja"/>
+    <Section name="Doświadczenie"/>
+    <Section name="Kursy"/>
+	</StyledMain>
+);
