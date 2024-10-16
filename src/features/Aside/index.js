1f0185524d65464f21891b622ea6f9e4305738b0
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 16:16:48 +0200

    added Aside component with sections

---
+++ src/features/Aside/index.js
@@ -0,0 +1,10 @@
+import { Section } from "../../common/Section";
+import { StyledAside } from "./styled";
+
+export const Aside = () => (
+	<StyledAside>
+		<Section name="Umiejętności" />
+    <Section name="Mocne strony" />
+    <Section name="Jak pracuję" />
+	</StyledAside>
+);
