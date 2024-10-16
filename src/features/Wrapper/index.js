ce3b7622ef70dad08ccb9050c70508e79e6086f1
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 16:15:26 +0200

    added Aside component to Wrapper

--- src/features/Wrapper/index.js
+++ src/features/Wrapper/index.js
@@ -1,10 +1,12 @@
 import { StyledWrapper } from "./styled";
 import { Header } from "../Header";
 import { Main } from "../Main";
+import { Aside } from "../Aside";
 
 export const Wrapper = () => (
 	<StyledWrapper>
 		<Header />
 		<Main />
+		<Aside />
 	</StyledWrapper>
 );
