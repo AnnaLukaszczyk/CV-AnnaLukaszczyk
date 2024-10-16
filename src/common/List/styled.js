e7fd75a95226d4680b1cd805e02d7be3c70284a7
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-16 17:10:07 +0200

    added styles for StyledList and ListItem

---
+++ src/common/List/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledList = styled.ul`
+	margin-left: 15px;
+	list-style: none;
+`;
+
+export const ListItem = styled.li`
+	display: flex;
+	align-items: center;
+`;
