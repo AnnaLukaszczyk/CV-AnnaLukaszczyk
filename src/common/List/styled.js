b77d5fdaf3a4b58c5036e0ba23edcaa7309f3882
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-17 17:33:41 +0200

    refactored the styled components to enhance the StyledList and ListItem styles, added a new Icon styled component

--- src/common/List/styled.js
+++ src/common/List/styled.js
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const StyledList = styled.ul`
-	margin-left: 15px;
+	margin: 12px 4px 0 12px;
 	list-style: none;
 `;
 
 export const ListItem = styled.li`
 	display: flex;
-	align-items: center;
+	align-items: flex-start;
+	margin: 5px 0;
+	font-size: 15px;
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+	margin: 6px 6px 0 0;
+	height: 6px;
+	color: #2e7186;
 `;
