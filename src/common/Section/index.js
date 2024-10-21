da8bd47d94600c3e05979c97254e7f60f9a66584
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-21 20:59:36 +0200

    updated Section component to pass down icons visibility

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,11 +1,11 @@
 import { List } from "../List";
 import { SectionTitle, SectionTitleShadow, StyledSection } from "./styled";
 
-export const Section = ({ name, isMain, content }) => (
+export const Section = ({ name, isMain, content, showIcons = true }) => (
 	<StyledSection>
-		<SectionTitleShadow>
-			<SectionTitle $isMain={isMain}>{name}</SectionTitle>
-		</SectionTitleShadow>
-		<List items={content} />
+			<SectionTitleShadow>
+				<SectionTitle $isMain={isMain}>{name}</SectionTitle>
+			</SectionTitleShadow>
+		<List items={content} isMain={isMain} showIcons={showIcons} />
 	</StyledSection>
 );
