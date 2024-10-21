86160a36dce2f468673817212a3ed4866bcb4176
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-21 19:16:11 +0200

    added a link to the course completion certificate in the CertificateLink component

---
+++ src/common/Links/index.js
@@ -0,0 +1,10 @@
+import { Link } from "./styled";
+
+export const CertyficateLink = () => (
+	<Link
+		href="https://www.linkedin.com/in/anna-%C5%82ukaszczyk2/overlay/1727812040650/single-media-viewer/?profileId=ACoAAD_VG6kBATOipEwAGeZSTdZYErWbb5B6V3A"
+		target="_blank"
+		rel="noopener noreferrer">
+		zobacz certyfikat ukończenia
+	</Link>
+);
