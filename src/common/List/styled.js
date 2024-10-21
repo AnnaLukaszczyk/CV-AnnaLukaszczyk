f8bcc8c70b2156050fcb8787be4a3fd6616f3a24
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-21 19:21:16 +0200

    updated list styling: improved margins, added line-height and dynamic icon color depending on isMain

--- src/common/List/styled.js
+++ src/common/List/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const StyledList = styled.ul`
-	margin: 12px 4px 0 12px;
+	margin: 12px 10px 0 10px;
 	list-style: none;
 `;
 
@@ -11,10 +11,11 @@ export const ListItem = styled.li`
 	align-items: flex-start;
 	margin: 5px 0;
 	font-size: 15px;
+	line-height: 1.4;
 `;
 
 export const Icon = styled(FontAwesomeIcon)`
 	margin: 6px 6px 0 0;
 	height: 6px;
-	color: #2e7186;
+	color: ${({ $isMain }) => ($isMain ? "rgba(93, 93, 93, 1)" : "#2e7186")};
 `;
