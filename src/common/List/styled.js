093c64497178a30fd2c0fb20106c98ba7fcc840c
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-21 20:57:28 +0200

    changed margin for StyledList and ListItem

--- src/common/List/styled.js
+++ src/common/List/styled.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const StyledList = styled.ul`
-	margin: 12px 10px 0 10px;
+	margin: 10px 10px 0 10px;
 	list-style: none;
 `;
 
 export const ListItem = styled.li`
 	display: flex;
 	align-items: flex-start;
-	margin: 5px 0;
+	margin: 3px 0;
 	font-size: 15px;
 	line-height: 1.4;
 `;
