975b6f7bfac4b6c79956b5bc06da622929e411f8
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-21 20:55:12 +0200

    added PersonalHomepage

--- src/common/Links/index.js
+++ src/common/Links/index.js
@@ -4,7 +4,19 @@ export const CertyficateLink = () => (
 	<Link
 		href="https://www.linkedin.com/in/anna-%C5%82ukaszczyk2/overlay/1727812040650/single-media-viewer/?profileId=ACoAAD_VG6kBATOipEwAGeZSTdZYErWbb5B6V3A"
 		target="_blank"
-		rel="noopener noreferrer">
+		rel="noopener noreferrer"
+		title="Kliknij, aby zobaczyć certyfikat">
 		zobacz certyfikat ukończenia
 	</Link>
 );
+
+
+export const PersonalHomepage = () => (
+	<Link
+	href="https://annalukaszczyk.github.io/Personal-homepage/"
+	target="_blank"
+	rel="noopener noreferrer"
+	title="Kliknij, aby przejść do strony o mnie, gdzie znajdziesz moje portfolio">
+	moim portfolio
+</Link>
+)
