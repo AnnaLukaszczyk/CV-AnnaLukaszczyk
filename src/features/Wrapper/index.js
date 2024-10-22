f5ba95de4e8769222942ed48ee7ac307300dbd96
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-22 13:17:18 +0200

    added Footer component to Wrapper

--- src/features/Wrapper/index.js
+++ src/features/Wrapper/index.js
@@ -2,11 +2,13 @@ import { StyledWrapper } from "./styled";
 import { Header } from "../Header";
 import { Main } from "../Main";
 import { Aside } from "../Aside";
+import { Footer } from "../Footer";
 
 export const Wrapper = () => (
 	<StyledWrapper>
 		<Header />
 		<Main />
 		<Aside />
+		<Footer />
 	</StyledWrapper>
 );
