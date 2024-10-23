9f34ee59ad38c43807c22909f1f75e8d9e0a4722
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-23 15:11:14 +0200

    added InfoBox with button and text to footer

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -1,19 +1,31 @@
-import { Layers, LayerBox, Layer1, Layer2, Layer3, StyledFooter } from "./styled";
+import {
+	Layers,
+	LayerBox,
+	Layer1,
+	Layer2,
+	Layer3,
+	StyledFooter,
+	Info,
+	InfoBox,
+	Button,
+} from "./styled";
 
 export const Footer = () => (
 	<StyledFooter>
-		<div>
-			<Layers>
-				<LayerBox>
-					<Layer1 />
-				</LayerBox>
-				<LayerBox>
-					<Layer2 />
-				</LayerBox>
-				<LayerBox>
-					<Layer3 />
-				</LayerBox>
-			</Layers>
-		</div>
+		<Layers>
+			<LayerBox>
+				<Layer1 />
+			</LayerBox>
+			<LayerBox>
+				<Layer2 />
+			</LayerBox>
+			<LayerBox>
+				<Layer3 />
+			</LayerBox>
+		</Layers>
+		<InfoBox>
+			<Button>?</Button>
+			<Info>Wyrażam zgodę na przetwarzanie moich danych osobowych zawartych w CV  w celu prowadzenia rekrutacji zgodnie z przepisami ustawy z dnia 10 maja 2018 roku o ochronie danych osobowych (Dz. U. z 2018 r. poz. 1000) oraz zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. (RODO).</Info>
+		</InfoBox>
 	</StyledFooter>
 );
