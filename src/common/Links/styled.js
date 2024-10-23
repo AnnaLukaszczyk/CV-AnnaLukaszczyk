c7ccf6235c8cbde6da4a950d0ee93475a8011330
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-23 17:17:57 +0200

    added blinkAnimation to Link

--- src/common/Links/styled.js
+++ src/common/Links/styled.js
@@ -1,10 +1,20 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const blinkAnimation = keyframes`
+  0%, 100% {
+    color: black;
+  }
+  50% {
+    color: #397c90bb;
+  }
+`;
 
 export const Link = styled.a`
 color: black;
 font-weight: bold;
 text-decoration: none;
+animation: ${blinkAnimation} 4s infinite;
 `
 
 export const ContactLink = styled.a`
