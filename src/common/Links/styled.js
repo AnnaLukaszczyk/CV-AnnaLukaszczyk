aed5500b97b723f65a94f59f75c9d433a48707b1
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-23 16:46:39 +0200

    added styles for ContactLink and Icon, added import for FontAwesomeIcon

--- src/common/Links/styled.js
+++ src/common/Links/styled.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export const Link = styled.a`
 color: black;
 font-weight: bold;
 text-decoration: none;
 `
+
+export const ContactLink = styled.a`
+color: azure;
+text-decoration: none;
+`
+
+export const Icon = styled(FontAwesomeIcon)`
+	margin-right: 10px;
+	filter: drop-shadow(1px 1px 2px rgb(26, 26, 26));
+`;
