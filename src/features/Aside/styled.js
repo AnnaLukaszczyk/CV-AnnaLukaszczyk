18522d4d4d8817f414cee8ac8026f64ff3e04985
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-29 17:44:30 +0100

    added padding-bottom in StyledAside

--- src/features/Aside/styled.js
+++ src/features/Aside/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledAside = styled.aside`
 	padding-top: 230px;
+	padding-bottom: 60px;
 	height: 100%;
 	width: 35%;
 	box-shadow: 2px 0 3px gray;
