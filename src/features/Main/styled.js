71e8694fba0737ec3cde76c81e8be1a63cb1c703
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-10-29 17:44:47 +0100

    added padding-bottom in StyledMain

--- src/features/Main/styled.js
+++ src/features/Main/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledMain = styled.main`
 	padding-top: 200px;
+	padding-bottom: 100px;
 	height: 100%;
 	width: 65%;
 	background-color: #e1e1e1;
